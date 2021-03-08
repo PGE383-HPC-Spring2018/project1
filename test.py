70223c670940717d2250fa725b1fef4017a8cabc
John T. Foster <john.foster@utexas.edu>
2021-03-08 12:32:39 -0600

    Update for 2021

--- test.py
+++ test.py
@@ -54,17 +54,6 @@ class TestSolution(unittest.TestCase):
         np.testing.assert_allclose(ans2, np.array([-5.88784058, -5.88784058, 13.]), atol=0.001) 
 
         
-    def test_Lorenz_private(self):
-        
-        sol = Lorenz(10, 8/3., 28)
-        ans1 = sol.solve(0)[:4]
-        ans2 = sol.solve(0)[-1]
-        np.testing.assert_allclose(ans1, np.array([[0.00000000e+00, 1.00000000e+00, 0.00000000e+00],
-           [9.51319372e-02, 1.00353668e+00, 4.79098355e-04],
-           [1.82791662e-01, 1.03242460e+00, 1.86879673e-03],
-           [2.66004060e-01, 1.08475943e+00, 4.16815796e-03]]), atol=0.001) 
-        np.testing.assert_allclose(ans2, np.array([ 6.38177035, 10.07082235, 16.93460356]), atol=0.001) 
-
     def test_plot(self):
        with warnings.catch_warnings():
            warnings.simplefilter("ignore")
